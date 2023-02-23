6ccc1be7b984a74ccee19b0e4302a8047f65b86f
Lukasz Serich <fargos990@gmail.com>
2023-02-23 18:53:52 +0100

    Fin

---
+++ main.js
@@ -0,0 +1,25 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+document.querySelector("h1").onmouseover = event=>
+{
+    let iterations = 0;
+
+    const interval = setInterval(()=>{
+    event.target.innerText = event.target.innerText.split("")
+    .map((letter,index) => 
+    {
+        if(index < iterations)
+        {
+            return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)]
+    })
+    .join("");
+
+    if(iterations >= event.target.dataset.value.length) clearInterval(interval);
+
+    iterations+= 1 / 5;
+
+    }, 30);
+}
